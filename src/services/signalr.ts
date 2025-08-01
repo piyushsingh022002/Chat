eb3e0a2fd01ff71d5f5569d7f2c1b10f0b79bd29
piyush singh <piyushsingh6521@gmail.com>
2025-08-01 17:25:36 +0530

    signal r added

---
+++ src/services/signalr.ts
@@ -0,0 +1,48 @@
+import * as signalR from "@microsoft/signalr";
+
+let connection: signalR.HubConnection;
+
+export const connectToChat = async (token: string) => {
+  connection = new signalR.HubConnectionBuilder()
+    .withUrl(`${process.env.REACT_APP_SIGNALR_URL}?access_token=${token}`)
+    .withAutomaticReconnect()
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+  connection.onclose((e) => {
+    console.warn("SignalR disconnected", e);
+  });
+
+  await connection.start();
+  console.log("SignalR connected");
+};
+
+export const sendMessage = async (message: string) => {
+  if (connection) {
+    await connection.invoke("SendMessage", message);
+  }
+};
+
+export const joinRoom = async (room: string) => {
+  if (connection) {
+    await connection.invoke("JoinRoom", room);
+  }
+};
+
+export const onMessageReceived = (cb: (user: string, message: string) => void) => {
+  if (connection) {
+    connection.on("ReceiveMessage", cb);
+  }
+};
+
+export const onUserJoined = (cb: (user: string) => void) => {
+  if (connection) {
+    connection.on("UserJoined", cb);
+  }
+};
+
+export const onUserLeft = (cb: (user: string) => void) => {
+  if (connection) {
+    connection.on("UserLeft", cb);
+  }
+};
