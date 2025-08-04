a00a3098078387ae5a7ca87c7e7278a6df85038a
piyush singh <piyushsingh6521@gmail.com>
2025-08-04 12:48:31 +0530

    using Browser Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
